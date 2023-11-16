f4da83261b43a1096426e30d8a2c9b22014b9d94
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-16 19:25:29 +0200

    initial project setup

---
+++ IteratorsAndCollections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IteratorsAndCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
